3ec43554244c899bbc87c8cd43b73df18ac0f9d7
xiaoyu <zytuatua@163.com>
2025-09-04 23:56:42 +0800

    feat:project struc init

---
+++ note-helper/src/main/java/com/zy/ai/notehelper/NoteHelperApplication.java
@@ -0,0 +1,13 @@
+package com.zy.ai.notehelper;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteHelperApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NoteHelperApplication.class, args);
+    }
+
+}
---
+++ note-helper/src/test/java/com/zy/ai/notehelper/NoteHelperApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zy.ai.notehelper;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NoteHelperApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
